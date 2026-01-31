5551d9dcd5ffd652ead6ebad2c7450a25eb543af
tofu-dev0123 <m.komukai0123@gmail.com>
2026-01-31 22:09:07 +0900

    設定ファイルの修正

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   event = { "BufReadPost", "BufNewFile" },
-  main = "nvim-treesitter.configs",
+  main = "nvim-treesitter.config",
   opts = {
     highlight = { enable = true },
   },
