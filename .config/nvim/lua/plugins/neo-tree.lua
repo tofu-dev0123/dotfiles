155805a0a20ba03d75d42ba32b5c88b71d18758b
masatokomukai <com12300000@gmail.com>
2026-02-04 16:33:35 +0900

    neotreeでパスをコピーするコマンドの設定を追加

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -15,6 +15,23 @@ return {
         filtered_items = {
           hide_dotfiles = false,
         },
+        window = {
+          mappings = {
+            ["<leader>yp"] = function(state)
+              local node = state.tree:get_node()
+              local path = node.path
+              local cwd = vim.loop.cwd()
+              local rel = path:gsub(cwd .. "/", "")
+              vim.fn.setreg("+", rel)
+              print("Copied: " .. rel)
+            end,
+            ["<leader>yP"] = function(state)
+              local node = state.tree:get_node()
+              vim.fn.setreg("+", node.path)
+              print("Copied: " .. node.path)
+            end,
+          },
+        },
       },
       window = {
         position = "left",
