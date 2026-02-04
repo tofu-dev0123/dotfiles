83d4a18b1ec08a543036e8906297fdeaf7c5d6cf
tofu-dev0123 <m.komukai0123@gmail.com>
2026-02-04 22:02:43 +0900

    マークダウンのnプレビューのプラグインを追加

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -2,6 +2,7 @@ local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
+  { import = "plugins.markdown-preview" },
   { import = "plugins.colorscheme" },
   { import = "plugins.lualine" },
   { import = "plugins.neo-tree" },
