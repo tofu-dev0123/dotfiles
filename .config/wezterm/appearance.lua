37034b8466766188708a4e356785ba2bbff9263b
masatokomukai <com12300000@gmail.com>
2026-02-05 11:38:50 +0900

    フォントの設定を追加

--- .config/wezterm/appearance.lua
+++ .config/wezterm/appearance.lua
@@ -1,6 +1,11 @@
 local M = {}
 
-function M.apply(config)
+function M.apply(config, wezterm)
+	config.font = wezterm.font_with_fallback {
+		"JetBrains Mono",
+		"HackGen",
+	}
+	config.font_size = 13.0
 	config.window_background_opacity = 0.8
         -- config.macos_window_background_blur = 10
 	config.window_decorations = "RESIZE"
