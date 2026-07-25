e67422e9c3c58a07a239f0a4a556944d4312f109
tofu-dev0123 <m.komukai0123@gmail.com>
2026-07-25 09:28:52 +0900

    chore: Wezterm の背景透過率を 1（不透明）に変更

--- wezterm/.config/wezterm/appearance.lua
+++ wezterm/.config/wezterm/appearance.lua
@@ -7,7 +7,7 @@ function M.apply(config, wezterm)
   }
   config.font_size = 13.0
   config.line_height = 1.3
-  config.window_background_opacity = 0.9
+  config.window_background_opacity = 1
   -- config.macos_window_background_blur = 10
   config.window_decorations = "RESIZE"
   config.window_frame = {
